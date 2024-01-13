1e2df55a890fa602f55e0a38100e8227429a4116
begpan <begumpandora@gmail.com>
2024-01-13 23:15:51 +0300

    recipe section added

--- main.js
+++ main.js
@@ -1,102 +1,91 @@
-import { renderResults } from './scripts/ui.js';
-import Search from '/scripts/search.js';
-import {ele, notify, renderLoader} from '/scripts/ui.js';
-import { categories } from './scripts/constant.js';
-import Recipe from './scripts/recipe.js';
-
+import { renderResults } from "./scripts/ui.js";
+import Search from "/scripts/search.js";
+import { ele, notify, renderLoader } from "/scripts/ui.js";
+import { categories } from "./scripts/constant.js";
+import Recipe from "./scripts/recipe.js";
 
 // classın ornegini oluşturma
 
-const search = new Search ();
-const recipe = new Recipe ();
+const search = new Search();
+const recipe = new Recipe();
 console.log(recipe);
 
 //! olay izleyicileri
 
 // sayfa yuklenme olayını izler
-document.addEventListener("DOMContentLoaded", ()=>{
-    // rastegele categori seç
-    const index = Math.floor(Math.random() * categories.length)
-    // kategorinin verilerini al ekrana bas
-   getResults(categories[index])
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  // rastegele categori seç
+  const index = Math.floor(Math.random() * categories.length);
+  // kategorinin verilerini al ekrana bas
+  getResults(categories[index]);
+});
 
 // formun gonderilme olayını izler
-ele.form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const query = e.target[0].value;
-    getResults(query);
-})
+ele.form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const query = e.target[0].value;
+  getResults(query);
+});
 
 // sayfa yuklenme olayını izle
 
-window.addEventListener("DOMContentLoaded",controlUrl)
+window.addEventListener("DOMContentLoaded", controlUrl);
 // urldeki id nin değişme olayını izle
 
+window.addEventListener("hashchange", controlUrl);
 
-window.addEventListener("hashchange" ,controlUrl)
+// tarif alanındaki tıklanma olaylarını izle
+
+ele.recipe_area.addEventListener("click", handleClick);
 
 //! fonksiyonlar
 // arama sonuclarını alıp ekrana basar
 
 async function getResults(query) {
-    if(!query.trim()){
-      return  notify("arama terimi giriniz")
+  if (!query.trim()) {
+    return notify("arama terimi giriniz");
+  }
+  renderLoader(ele.result_list);
+
+  try {
+    // apiden verileri al
+
+    await search.fetchResults(query.trim());
+
+    if (search.result.error) {
+      notify("aradığığnız kriterlere uygun urun bulunamadı");
+      // veriler hatalı geldiğinde loaderi kaldır
+      ele.result_list.innerHTML = "";
+    } else {
+      renderResults(search.result.recipes);
     }
-    renderLoader(ele.result_list)
 
-    try{
-         // apiden verileri al
-
-        await search.fetchResults(query.trim())
-
-        if(search.result.error){
-            notify('aradığığnız kriterlere uygun urun bulunamadı')
-            // veriler hatalı geldiğinde loaderi kaldır
-            ele.result_list.innerHTML = '';
-
-
-
-        } else{
-            renderResults(search.result.recipes)
-        }           
-        
-
-
-            // sonucları ekrana bas
-
-    }
-    
+    // sonucları ekrana bas
+  } catch (err) {
     // hata olursa bildirim ver
-    catch(err){
-        
-        notify("bir sorun oluştu")
+    notify("bir sorun oluştu");
 
-        ele.result_list.innerHTML = '';
-    }
-   
- 
-
-
-
+    ele.result_list.innerHTML = "";
+  }
 }
 
 // detay verilerini alıp ekrana basar
-async function controlUrl(){
-    // detayı gosterilecek ürün id sine eriş
-   const id = location.hash.slice(1)
+async function controlUrl() {
+  // detayı gosterilecek ürün id sine eriş
+  const id = location.hash.slice(1);
 
-    if(id){
-        // yğkleniyor bas
-        renderLoader(ele.recipe_area)
+  if (id) {
+    // yğkleniyor bas
+    renderLoader(ele.recipe_area);
 
-        // tarif bilgilerini al
+    // tarif bilgilerini al
 
-        await recipe.getRecipe(id)
-        
-        recipe.renderRecipe(recipe.info)
-    }
+    await recipe.getRecipe(id);
 
+    recipe.renderRecipe(recipe.info);
+  }
 }
-     
+
+// tarif alanındaki tıklamalarda calısır
+
+function handleClick(e) {}
--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,63 +1,80 @@
-import { ele } from "./ui.js"
+import { ele } from "./ui.js";
 
 export default class Recipe {
-    constructor(){
-        // tarif hakkındaki bilgiler
-        this.info = {}
-    }
+  constructor() {
+    // tarif hakkındaki bilgiler
+    this.info = {};
+  }
 
-    // apiden tarif bilgilerni alan metot
-    
-    async getRecipe(id){
-      const res =  await fetch (
-        `https://forkify-api.herokuapp.com/api/get?rId=${id}`
-        )
+  // apiden tarif bilgilerni alan metot
 
-        const data = await res.json()
-        // bilgileri class a tanımlama
-        this.info = data.recipe
+  async getRecipe(id) {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/get?rId=${id}`
+    );
 
-    }
-}
+    const data = await res.json();
+    // bilgileri class a tanımlama
+    this.info = data.recipe;
+  }
 
-// tarif bilgilerini ekrana basan method
-renderRecipe(r) {
-    ele.recipe_area.innerHTML =
-`
-<!-- resim alanı -->
-          <!-- <figure>
-            <img src="images/download.png" alt="">
-            <h1>Chicken title</h1>
-            <div class="like-area">
-              <i class="bi bi-heart"> </i>
+  // tarif bilgilerini ekrana basan method
+  renderRecipe(r) {
+    // tarif bilgilerini ekrana basan method
+
+    ele.recipe_area.innerHTML = `
+    <!-- resim alanı -->
+
+    <figure>
+        <img src="${r.image_url}" />
+        <h1>${r.title}</h1>
+        <div class="like-area">
+          <i id="like-btn" class="bi bi-heart"> </i>
+        </div>
+    </figure>
+            <!-- tarif alanı -->
+  
+           <div class="ingredients">
+              <ul>
+             ${this.createIngredient()}
+              </ul>
             </div>
-          </figure> -->
-          <!-- tarif alanı -->
+  
+            <!-- nasıl pişirilir -->
+            <div class="directions">
+              <h2>Nasıl Pişirilir</h2>
+              <p>Lorem ipsum dolor <span>${
+                r.publisher
+              }</span>sit adipisicing elit. Aspernatur autem nisi corporis assumenda quisquam labore fugit nesciunt. Voluptatum possimus, tempore deserunt architecto commodi quibusdam dolorem id quo officia maxime ipsum?</p>
+              <a href="/adsd" target="_blank">Yönerge</a>
+            </div>
+  
+    
+  `;
+    // gerekli olay izleyicisi ekle
+    document
+      .querySelector("#like-btn")
+      .addEventListener("click", () => this.controlLike());
+  }
 
-          <!-- <div class="ingredients">
-            <ul>
-              <li>
-                <i class=""></i>
-                <p>Salt and Pepdfafafafaffsfadfaper</p>
-              </li>
-              <li>
-                <i class=""></i>
-                <p>Salt and Peppesdadaardsfsdfsfsfdfdfsdfdsfdfdfdffsdsf</p>
-              </li>
-              <button id="add-to-cart" class="CartBtn">
-                <span class="IconContainer"> 
-                  <i class="cart bi bi-cart-fill"></i>
-                </span>
-                <p class="text">Add to Cart</p>
-              </button>
-            </ul>
-          </div> -->
+  // her bir malszeme için li elemanı oluşturur
 
-          <!-- nasıl pişirilir -->
-          <!-- <div class="directions">
-            <h2>Nasıl Pişirilir</h2>
-            <p>Lorem ipsum dolor <span>amet consectetur </span>sit adipisicing elit. Aspernatur autem nisi corporis assumenda quisquam labore fugit nesciunt. Voluptatum possimus, tempore deserunt architecto commodi quibusdam dolorem id quo officia maxime ipsum?</p>
-            <a href="/adsd" target="_blank">Yönerge</a>
-          </div> -->
+  createIngredient() {
+    return this.info.ingredients
+      .map(
+        (i) => `
+    <li>
+      <i class=""></i>
+      <p>${i}</p>
+    </li>
+    
+    `
+      )
+      .join("");
+  }
 
-`}
+  // eleman likelanmışsa like  kaldır yoksa likela
+  controlLike() {
+    console.log("tıklandı", this.info);
+  }
+}
